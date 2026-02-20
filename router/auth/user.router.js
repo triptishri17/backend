429977a44790704ec1e82f6b955a05339521c890
Tripti Shrivastav <shrivastavtripti5472@gmail.com>
2026-02-20 15:10:55 +0530

    Fix/update user routes

--- router/auth/user.router.js
+++ router/auth/user.router.js
@@ -190,7 +190,7 @@ router.post("/login", async (req, res) => {
 
     const token = jwt.sign(
       { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
+      "12345dfdfd_%$%dfgdfg",
       { expiresIn: "7d" }
     );
 
